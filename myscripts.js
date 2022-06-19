a53ae9780a21f41c11b1a814345eed0dfbdebf8a
Suraj SK <surajsk2002@gmail.com>
2022-06-19 13:19:31 +0530

    new commit

--- myscripts.js
+++ myscripts.js
@@ -2,4 +2,14 @@ const celsiusInput = document.getElementById("celsius");
 const fahrenheitInput = document.getElementById("fahrenheit");
 const kelvin = document.getElementById("kelvin");
 
-const inputs = document.getElementsByClassName("inputs");
+const inputs = document.getElementsByClassName("input");
+
+for (let i =0;i< inputs.length; i++){
+    let input = inputs[i];
+
+        input.addEventListener("input", function(e){
+            let value = e.target.value;
+            console.log(e.target.name + ":" + value);
+        });
+
+}
